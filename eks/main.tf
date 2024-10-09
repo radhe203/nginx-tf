8081d32a4eefdd89544e393fafdb841dbb24ca9b
radheshyam <radheshyamjio52@gmail.com>
2024-10-09 20:36:00 +0530

    complete iac for eks cluste

---
+++ eks/main.tf
@@ -0,0 +1,107 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "eks-vpc"
+  cidr = var.cidr
+
+  azs = data.aws_availability_zones.azs.names
+  #   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = var.public_subnets
+  private_subnets = var.public_subnets
+
+  #   enable_nat_gateway = true
+  #   enable_vpn_gateway = true
+
+  enable_dns_hostnames = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+
+  tags = {
+    Terraform                              = "true"
+    Environment                            = "dev"
+    Name                                   = "eks-vpc"
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"               = 1
+  }
+  private_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"      = 1
+  }
+}
+
+
+#  security group 
+
+module "sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "jenkins-server-sg"
+  description = "Security group for jenkin"
+  vpc_id      = module.vpc.vpc_id
+
+  #   ingress_cidr_blocks      = ["10.10.0.0/16"]
+  #   ingress_rules            = ["https-443-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      description = "HTTp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "SSH"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  tags = {
+    Name = "Jenkins-server-sg"
+  }
+}
+
+
+# creating ec2
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "nginx-tf"
+  cluster_version = "1.30"
+
+  cluster_endpoint_public_access = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    nodes = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+      instance_type = ["t2.micro"]
+    }
+  }
+
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
---
+++ eks/variable.tf
@@ -0,0 +1,13 @@
+variable "cidr" {
+  description = "vpc cidr"
+}
+
+variable "private_subnets" {
+  description = "Subnets CIDR"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "Subnets CIDR"
+  type        = list(string)
+}
